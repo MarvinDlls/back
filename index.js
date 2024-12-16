8b91031495ec447a40ae1835de4368d7509e6a68
Marvindlls <marvindillenseger@gmail.com>
2024-12-16 14:58:32 +0100

    enhance MongoDB connection and CORS configuration with improved comments and origin handling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const recipeRoute = require('./routes/recipe');
 const infoRoutes = require('./routes/api');
 
+// Connexion à MongoDB
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/recipe";
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
@@ -17,27 +18,32 @@ mongoose.connect(MONGO_URI, {
         console.error("Erreur de connexion à MongoDB :", error);
     });
 
+// Liste des origines autorisées (ajoute ton URL de production ici)
 const allowedOrigins = [
     'http://localhost:5173', // Local
-    'https://projet-ecf-marvin.vercel.app/' // Vercel
+    'https://projet-ecf-marvin.vercel.app', // Vercel
 ];
 
+// Configuration de CORS
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
+            callback(null, true);  // Autoriser l'origine
         } else {
-            callback(new Error('Non autorisé par CORS'));
+            callback(new Error('Non autorisé par CORS'));  // Refuser l'origine
         }
     }
 }));
 
+// Middleware pour analyser les corps de requêtes JSON et URL encodés
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use("/recipe", recipeRoute);
 app.use("/api", infoRoutes);
 
+// Démarrage du serveur
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`L'API est lancée sur l'url http://localhost:${PORT}`);
